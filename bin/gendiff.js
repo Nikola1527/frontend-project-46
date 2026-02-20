4db06346adbefeeb9ad986cbac23ff5dc4cf35c1
Nikola1527 <kolyan.lysakovv@mail.ru>
2026-02-20 18:31:10 +0300

    3 шаг проекта

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,7 @@
 import { Command } from "commander";
 import genDiff from "../src/index.js";
+import path from "node:path";
+import process from "node:process";
 
 const program = new Command();
 
@@ -11,9 +13,17 @@ program
   .argument("<filepath1>", "path to first configuration file")
   .argument("<filepath2>", "path to second configuration file")
   .option("-f, --format [type]", "output format")
-  .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2, option.format);
-    console.log(diff);
+  .action((filepath1, filepath2, options) => {
+    try {
+      const absolutePath1 = path.resolve(process.cwd(), filepath1);
+      const absolutePath2 = path.resolve(process.cwd(), filepath2);
+
+      const diff = genDiff(absolutePath1, absolutePath2);
+      console.log(diff);
+    } catch (e) {
+      console.e("Error:", e.message);
+      process.exit(1);
+    }
   });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
+import fs from "node:fs";
+import path from "node:path";
+
 const genDiff = (filepath1, filepath2) => {
-  return `Разница ${filepath1} и ${filepath2}`;
+  try {
+    const data1 = fs.readFileSync(filepath1, "utf8");
+    const data2 = fs.readFileSync(filepath2, "utf8");
+
+    const ext1 = path.extname(filepath1);
+    const ext2 = path.extname(filepath2);
+
+    if (ext1 !== ".json" || ext2 !== ".json") {
+      throw new Error("Only JSON files are supported for now");
+    }
+
+    const obj1 = JSON.parse(data1);
+    const obj2 = JSON.parse(data2);
+
+    return JSON.stringify({ obj1, obj2 }, null, 2);
+  } catch (e) {
+    throw new Error(`Error reading files: ${e.message}`);
+  }
 };
 
 export default genDiff;
