a803b4196686f231bbed87f12e185d11684561ee
Nikola1527 <kolyan.lysakovv@mail.ru>
2026-02-25 18:39:49 +0300

    shoot #4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
+import _ from "lodash";
 
 const genDiff = (filepath1, filepath2) => {
   try {
@@ -16,7 +17,34 @@ const genDiff = (filepath1, filepath2) => {
     const obj1 = JSON.parse(data1);
     const obj2 = JSON.parse(data2);
 
-    return JSON.stringify({ obj1, obj2 }, null, 2);
+    const buildDiff = (obj1, obj2) => {
+      const allKeys = _.union(Object.keys(obj1), Object.keys(obj2));
+      const sortKeys = _.sortBy(allKeys);
+
+      const lines = sortKeys.reduce((acc, key) => {
+        const hasInObj1 = _.has(obj1, key);
+        const hasInObj2 = _.has(obj2, key);
+
+        if (hasInObj1 && !hasInObj2) {
+          acc.push(`  - ${key}: ${obj1[key]}`);
+        } else if (!hasInObj1 && hasInObj2) {
+          acc.push(`  + ${key}: ${obj2[key]}`);
+        } else if (hasInObj1 && hasInObj2) {
+          if (_.isEqual(obj1[key], obj2[key])) {
+            acc.push(`    ${key}: ${obj1[key]}`);
+          } else {
+            acc.push(`  - ${key}: ${obj1[key]}`);
+            acc.push(`  + ${key}: ${obj2[key]}`);
+          }
+        }
+
+        return acc;
+      }, []);
+
+      return `{\n${lines.join("\n")}\n}`;
+    };
+
+    return buildDiff(obj1, obj2);
   } catch (e) {
     throw new Error(`Error reading files: ${e.message}`);
   }
